14993880a461049e8c26f230697c9aec8ee397b8
KianRK <101524407+KianRK@users.noreply.github.com>
2022-09-21 17:25:23 +0200

    added comments and changed code to Build_tower_Kata

--- Build_tower_Kata.py
+++ Build_tower_Kata.py
@@ -1,5 +1,13 @@
+#At first I did not like this Kata, because instructions and expexted return
+#respectively output have not been clear in my opinion.
+# But it taught me the string.center() method I did not know before.
+#below I used the center() method and commented out my previous solution for comparison.
+#Im aware its not best practice in terms of readability, but for a change I laid focus
+#compressing the code in this one.
+
 def tower_builder(n_floors):
-    pyramid = [(n_floors-(n+1))*" " + "*" + n*2*"*" + (n_floors-(n+1))*" " for n in range(n_floors)]
+    pyramid = [("*" + n*2*"*").center(2*n_floors-1)]
+    #pyramid = [(n_floors-(n+1))*" " + "*" + n*2*"*" + (n_floors-(n+1))*" " for n in range(n_floors)]
     return pyramid
 
 # task description:
