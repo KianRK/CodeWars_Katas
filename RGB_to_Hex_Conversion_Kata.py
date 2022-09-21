c750ba769851efbb30a5e6307290ab552ae3969e
KianRK <101524407+KianRK@users.noreply.github.com>
2022-09-21 18:59:05 +0200

    added comments to RGB_to_Hex_Conversion_Kata

--- RGB_to_Hex_Conversion_Kata.py
+++ RGB_to_Hex_Conversion_Kata.py
@@ -1,17 +1,16 @@
-def rgb_to_hex(rgb):
-    hex_chars = {10:"A", 11:"B", 12:"C", 13:"D", 14:"E", 15:"F"}
-    if rgb < 0:
+def rgb_to_hex(rgb):                                                        #creating method to convert a decimal between 0 and 255 into hexadecimal number
+    hex_chars = {10:"A", 11:"B", 12:"C", 13:"D", 14:"E", 15:"F"}            #mapping two digit numbers to the corresponding char in the hexadecimal base.
+    if rgb < 0:                                                             #cutting numbers below or above the accepted range
         rgb = 0
     if rgb > 255:
         rgb = 255
     rgb = round(rgb)
-    hex = hex_chars[rgb//16] if rgb//16 > 9 else str(rgb//16)
-    hex += hex_chars[(rgb%16)*1] if (rgb%16)*1 > 9 else str((rgb%16)*1)
-    return hex
+    single_hex = hex_chars[rgb//16] if rgb//16 > 9 else str(rgb//16)               #converting the decimal with the division method
+    single_hex += hex_chars[(rgb%16)*1] if (rgb%16)*1 > 9 else str((rgb%16)*1)     # replacing numbers above 9 with the corresponding char and concatenate them to a string.
+    return single_hex
 
 def rgb(r, g, b):
-    hex = rgb_to_hex(r) + rgb_to_hex(g) + rgb_to_hex(b)
-    # your code here :)
+    hex = rgb_to_hex(r) + rgb_to_hex(g) + rgb_to_hex(b)                     #simply concatenate the return values of the rgb_to_hex method with the different parameters to a string.
     return hex
 
 # task description:
